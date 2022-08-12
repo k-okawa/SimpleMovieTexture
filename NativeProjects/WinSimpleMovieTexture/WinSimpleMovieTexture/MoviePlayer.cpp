374ad71f44fe1ab413a437c5ee1da4413ef73710
Kyohei <kurokenken0310@gmail.com>
2022-08-12 15:26:47 +0900

    implement playing movie

--- NativeProjects/WinSimpleMovieTexture/WinSimpleMovieTexture/MoviePlayer.cpp
+++ NativeProjects/WinSimpleMovieTexture/WinSimpleMovieTexture/MoviePlayer.cpp
@@ -3,13 +3,13 @@
 
 MoviePlayer::MoviePlayer(char* filePath) {
 	HRESULT hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER, IID_IGraphBuilder, (void**)&_pGraph);
-	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Could not create the Filter Graph Manager");
+	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Could not create the Filter Graph Manager"); return;
 
 	hr = CoCreateInstance(CLSID_SampleGrabber, NULL, CLSCTX_INPROC, IID_IBaseFilter, (void**)&_pSampleGrabberFilter);
-	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Could not create Sample Grabber Filter.");
+	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Could not create Sample Grabber Filter."); return;
 
 	hr = _pSampleGrabberFilter->QueryInterface(IID_ISampleGrabber, (void**)&_pSampleGrabber);
-	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed getting ISampleGrabber.");
+	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed getting ISampleGrabber."); return;
 
 	ZeroMemory(&mediaType, sizeof(mediaType));
 	mediaType.majortype = MEDIATYPE_Video;
@@ -18,12 +18,16 @@ MoviePlayer::MoviePlayer(char* filePath) {
 	_pSampleGrabber->SetMediaType(&mediaType);
 
 	hr = _pGraph->AddFilter(_pSampleGrabberFilter, L"Sample Grabber");
-	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed add SampleGrabberFilter.");
+	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed add SampleGrabberFilter."); return;
 
 	hr = _pGraph->QueryInterface(IID_IMediaControl, (void**)&_pControl);
-	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed getting IMediaControl.");
+	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed getting IMediaControl."); return;
 
-	hr = _pGraph->RenderFile(L"", NULL);
+	hr = _pGraph->RenderFile(global::CharPtrToLPWSTR(filePath), NULL);
+	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed render file."); return;
+
+	hr = _pControl->Run();
+	if (FAILED(hr)) global::DebugLogError("SimpleMovieTexture - Failed play movie."); return;
 }
 
 MoviePlayer::~MoviePlayer() {
