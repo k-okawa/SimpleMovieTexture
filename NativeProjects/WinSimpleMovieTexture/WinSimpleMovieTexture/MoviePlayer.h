9b464640be92f40610a4806d03ada5d4c748547c
Kyohei <kurokenken0310@gmail.com>
2022-08-28 21:40:35 +0900

    clean movie player

--- NativeProjects/WinSimpleMovieTexture/WinSimpleMovieTexture/MoviePlayer.h
+++ NativeProjects/WinSimpleMovieTexture/WinSimpleMovieTexture/MoviePlayer.h
@@ -10,21 +10,7 @@ public:
 	~MoviePlayer();
 
 	void Init(char* filePath);
-	long* GetCurrentBuffer() {
-		return _pBuffer;
-	}
 private:
-	std::thread _thread;
-
 	std::unique_ptr<wchar_t[]> _moviePath;
-	AM_MEDIA_TYPE _mediaType;
-	long* _pBuffer = nullptr;
-
-	IGraphBuilder* _pGraph = nullptr;
-	IMediaControl* _pControl = nullptr;
-	IBaseFilter* _pSampleGrabberFilter = nullptr;
-	ISampleGrabber* _pSampleGrabber = nullptr;
-	IVideoWindow* _pVideoWindow = nullptr;
-	IMediaEvent* _pEvent = nullptr;
 };
 
--- NativeProjects/WinSimpleMovieTexture/WinSimpleMovieTexture/MovieTexture.h
+++ NativeProjects/WinSimpleMovieTexture/WinSimpleMovieTexture/MovieTexture.h
@@ -2,18 +2,20 @@
 
 #include <streams.h>
 #include <dshow.h>
+#include <memory>
 
 struct __declspec(uuid("{fa36726f-a7ec-4c3d-9bdf-5a00a5209b22}")) CLSID_TextureRenderer;
 
 class MovieTexture : CBaseVideoRenderer 
 {
+	std::unique_ptr<BYTE[]> pTextureBuffer;
 public:
 	MovieTexture(LPUNKNOWN pUnk, HRESULT* phr);
 	~MovieTexture();
 
 	HRESULT CheckMediaType(const CMediaType* pmt);     // Format acceptable?
 	HRESULT SetMediaType(const CMediaType* pmt);       // Video format notification
-	//HRESULT DoRenderSample(IMediaSample* pMediaSample); // New video sample
+	HRESULT DoRenderSample(IMediaSample* pSample); // New video sample
 
 	LONG lVidWidth;
 	LONG lVidHeight;
